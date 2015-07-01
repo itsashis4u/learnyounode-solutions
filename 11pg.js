26dadafa0753a640b19484cfac68f54a8783f70e
Ashish <itsashis4u@gmail.com>
2015-07-01 15:23:43 +0530

    Finished 11th submission

---
+++ 11pg.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function(req, res) {
+
+    fs.createReadStream(process.argv[3]).pipe(res);
+}).listen(process.argv[2] || 8000);
